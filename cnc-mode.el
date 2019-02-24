51491d2dbf3b55d5de081d16a908e0038a7d03c6
Matthijs Kool <matthijzk@gmail.com>
2019-02-24 19:37:30 +0100

    Add README.org

--- cnc-mode.el
+++ cnc-mode.el
@@ -1,5 +1,9 @@
 ;;; cnc-mode.el --- CNC mode -*- lexical-binding: t -*-
+;; TODO: add README.org
 ;; TODO: test on older Emacsen
+;; TODO: test with nc files from other machine vendors
+;; TODO: add unit tests using ert
+;; TODO: set up travis (or similar)
 
 ;; Copyright © 2019 Matthijs Kool
 
