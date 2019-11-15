36e5f81a3173a60c9de6096792b39717d8439244
Matthijs Kool <matthijzk@gmail.com>
2019-11-15 19:57:12 +0100

    Font lock line numbers with font-lock-constant-face
    Line numbers also function as labels.

--- cnc-mode.el
+++ cnc-mode.el
@@ -173,7 +173,8 @@ to the beginning of the closest preceding one."
     ("[G][0-9]+" . font-lock-keyword-face)
     ("\\([MTO]\\)-?[0-9]+" . (1 font-lock-builtin-face))
     ("\\([FSXYZPQRBIJK]\\)-?[0-9]+" . (1 font-lock-variable-name-face))
-    ("#\\(\\[\\|[0-9]+\\)" . font-lock-variable-name-face))
+    ("#\\(\\[\\|[0-9]+\\)" . font-lock-variable-name-face)
+    ("^N[0-9]*" . font-lock-constant-face))
   "Default expressions to highlight in CNC mode.")
 
 (defcustom cnc-mode-hook nil
